b9b22bfb0367cc24ddd137bcb91a46fdf51c9dc0
Tempeus <nekugm@gmail.com>
2020-02-02 00:22:01 -0500

    Added VisionAPI Code

--- app/src/main/java/com/example/test_project/MainActivity.java
+++ app/src/main/java/com/example/test_project/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.test_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
@@ -29,12 +33,24 @@ import com.google.api.services.vision.v1.model.Image;
 import com.google.api.services.vision.v1.model.ImageProperties;
 import com.google.api.services.vision.v1.model.SafeSearchAnnotation;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private static final int RECORD_REQUEST_CODE = 101;
     private static final int CAMERA_REQUEST_CODE = 102;
 
     private static final String CLOUD_VISION_API_KEY = "AIzaSyAd4M0IV9xzjWAM8eEzcPiBbsE_klVnSC8";
+
+    private Feature feature;
+    private Bitmap bitmap;
+    private String visionAPI = "LABEL_DETECTION";
+
+    TextView visionAPIData;
 
     ImageView imageView;
 
@@ -43,6 +59,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        feature = new Feature();
+        feature.setType(visionAPI);
+
+        //Setting the maximum results obtained by the API as 10 ingredients
+        feature.setMaxResults(10);
 
         Button btnCamera = (Button) findViewById(R.id.btnCamera);
         imageView = (ImageView)findViewById(R.id.imageView);
@@ -64,4 +86,96 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @NonNull
+    private Image getImageEncodeImage(Bitmap bitmap){
+        Image base64EncodedImage = new Image();
+        //convert the bitmap to JPEG
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+        base64EncodedImage.encodeContent(imageBytes);
+        return base64EncodedImage;
+    }
+
+    private void callVisionAPI(final Bitmap bitmap, final Feature feature){
+        //imageUploadProgress.setVisibility(View.VISIBLE);
+        final List<Feature> featureList = new ArrayList<>();
+        featureList.add(feature);
+
+        final List<AnnotateImageRequest> annotateImageRequests = new ArrayList<>();
+
+        AnnotateImageRequest annotateImageReq = new AnnotateImageRequest();
+        annotateImageReq.setFeatures(featureList);
+        annotateImageReq.setImage(getImageEncodeImage(bitmap));
+        annotateImageRequests.add(annotateImageReq);
+
+        new AsyncTask<Object, Void, String>() {
+            @Override
+            protected String doInBackground(Object... params) {
+                try {
+
+                    HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+                    JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+                    VisionRequestInitializer requestInitializer = new VisionRequestInitializer(CLOUD_VISION_API_KEY);
+
+                    Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
+                    builder.setVisionRequestInitializer(requestInitializer);
+
+                    Vision vision = builder.build();
+
+                    BatchAnnotateImagesRequest batchAnnotateImagesRequest = new BatchAnnotateImagesRequest();
+                    batchAnnotateImagesRequest.setRequests(annotateImageRequests);
+
+                    Vision.Images.Annotate annotateRequest = vision.images().annotate(batchAnnotateImagesRequest);
+                    annotateRequest.setDisableGZipContent(true);
+                    BatchAnnotateImagesResponse response = annotateRequest.execute();
+                    return convertResponseToString(response);
+                } catch (GoogleJsonResponseException e) {
+                    Log.d(TAG, "failed to make API request because " + e.getContent());
+                } catch (IOException e) {
+                    Log.d(TAG, "failed to make API request because of other IOException " + e.getMessage());
+                }
+                return "Cloud Vision API request failed. Check logs for details.";
+            }
+
+            protected void onPostExecute(String result) {
+                visionAPIData.setText(result);
+//                imageUploadProgress.setVisibility(View.INVISIBLE);
+            }
+        }.execute();
+    }
+
+    private String convertResponseToString(BatchAnnotateImagesResponse response) {
+        AnnotateImageResponse imageResponses = response.getResponses().get(0);
+        List<EntityAnnotation> entityAnnotations;
+        entityAnnotations = imageResponses.getLabelAnnotations();
+        String message = "";
+
+        if (entityAnnotations != null) {
+            //This message contains all the objects captured by the API as well as its score
+            for (EntityAnnotation entity : entityAnnotations) {
+                message = entity + "    " + entity.getDescription() + " " + entity.getScore();
+                message += "\n";
+            }
+        } else {
+            message = "Nothing Found";
+        }
+        return message;
+    }
+
+    private String formatAnnotation(List<EntityAnnotation> entityAnnotation) {
+        String message = "";
+
+        if (entityAnnotation != null) {
+            for (EntityAnnotation entity : entityAnnotation) {
+                message = message + "    " + entity.getDescription() + " " + entity.getScore();
+                message += "\n";
+            }
+        } else {
+            message = "Nothing Found";
+        }
+        return message;
+    }
 }
