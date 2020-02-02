4e673f4bacdc22a08c1e68ca38be469a1b0d10b1
Alexander-Cui <alexander.cui@mail.mcgill.ca>
2020-02-02 00:28:53 -0500

    camera

--- app/src/main/java/com/example/test_project/MainActivity.java
+++ app/src/main/java/com/example/test_project/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.test_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
 
+
+public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST =1001;
     
     ImageView imageView;
+    Bitmap bitmap;
 
 
     @Override
@@ -27,17 +33,43 @@ public class MainActivity extends AppCompatActivity {
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,0);
+                takePictureFromCamera();
             }
         });
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode,resultCode,data);
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-        imageView.setImageBitmap(bitmap);
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+//            callCloudVision(bitmap, feature);
+        }
+
+    }
+    @NonNull
+    private Image getImageEncodeImage (Bitmap bitmap ){
+        Image base64EncodedImage = new Image();
+        // Convert the bitmap to a JPEG
+        // Just in case it's a format that Android understands but Cloud Vision
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+        // Base64 encode the JPEG
+        base64EncodedImage.encodeContent(imageBytes);
+        return base64EncodedImage;
+    }
+
+
+
+    }
+
+    public void takePictureFromCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager())!=null) {
+            startActivityForResult(intent, CAMERA_REQUEST);
+        }
 
     }
 
