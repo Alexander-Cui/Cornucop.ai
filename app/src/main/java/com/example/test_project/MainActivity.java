292664edb7024870554a54e8748fec1ca75d0a86
Tempeus <nekugm@gmail.com>
2020-02-01 23:53:12 -0500

    Added Authentication key Added API imports

--- app/src/main/java/com/example/test_project/MainActivity.java
+++ app/src/main/java/com/example/test_project/MainActivity.java
@@ -10,9 +10,32 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.AnnotateImageResponse;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.ColorInfo;
+import com.google.api.services.vision.v1.model.DominantColorsAnnotation;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+import com.google.api.services.vision.v1.model.ImageProperties;
+import com.google.api.services.vision.v1.model.SafeSearchAnnotation;
+
 public class MainActivity extends AppCompatActivity {
 
-    
+    private static final int RECORD_REQUEST_CODE = 101;
+    private static final int CAMERA_REQUEST_CODE = 102;
+
+    private static final String CLOUD_VISION_API_KEY = "AIzaSyAd4M0IV9xzjWAM8eEzcPiBbsE_klVnSC8";
+
     ImageView imageView;
 
 
